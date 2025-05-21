7cbd58101b56e31e350cb754913e1869f1176f51
Nebiyu Daniel <nebiyu.daniel@stu.ocsb.ca>
2025-05-21 14:23:29 +0000

    optimized and favicon

--- js/game.js
+++ js/game.js
@@ -1,10 +1,26 @@
+// This file is part of the RST-ICD2O-DBZ-Aliens project
+// Copyright (C) 2025 RST-ICD2O-DBZ-Aliens
+// Made by Nebiyu Daniel
+// this is the phaser 3 configuration file
+
 /* global Phaser */
 
+//* Game scene */
 const config = {
     type: Phaser.AUTO,
     width: 1920,
     height: 1080,
+    physics: {
+        default: 'arcade',
+        arcade: {
+            debug: true
+        }
+    },
     backgroundColor: 0x5f6e7a,
+    scale: {
+        mode: Phaser.Scale.FIT,
+        autoCenter: Phaser.Scale.CENTER_BOTH,
+    },
 }
 
 const game = new Phaser.Game(config);
