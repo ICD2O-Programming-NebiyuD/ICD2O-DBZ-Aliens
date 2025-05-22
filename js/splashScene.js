824e4def86599e419e1139139cd910cbb596d195
Nebiyu Daniel <nebiyu.daniel@stu.ocsb.ca>
2025-05-22 14:02:44 +0000

    fixed

--- js/splashScene.js
+++ js/splashScene.js
@@ -23,7 +23,6 @@ class SplashScene extends Phaser.Scene {
      * Can be defined on your own Scenes.
      * This method is called by the Scene Manager when the scene starts,
      * before preload() and create().
-     * @param {object} data - Any data passed via ScenePlugin.add() or ScenePlugin.start().
      */
     init(data) {
       this.cameras.main.setBackgroundColor('#ffffff')
@@ -40,7 +39,6 @@ class SplashScene extends Phaser.Scene {
     /**
      * Can be defined on your own Scenes.
      * Use it to create your game objects.
-     * @param {object} data - Any data passed via ScenePlugin.add() or ScenePlugin.start().
      */
     create(data) {
       // pass
@@ -49,9 +47,7 @@ class SplashScene extends Phaser.Scene {
     /**
      * Should be overridden by your own Scenes.
      * This method is called once per game step while the scene is running.
-     * @param {number} time - The current time.
-     * @param {number} delta - The delta time in ms since the last frame.
-     */
+*/
     update(time, delta) {
       // pass
     }
