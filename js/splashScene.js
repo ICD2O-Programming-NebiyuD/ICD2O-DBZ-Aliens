1bad9d936f2e3d40dab49afc245f393f0915c360
Nebiyu Daniel <nebiyu.daniel@stu.ocsb.ca>
2025-05-22 14:27:27 +0000

    fixed

--- js/splashScene.js
+++ js/splashScene.js
@@ -1,37 +1,29 @@
-/* global Phaser */
-
-/*
- * Copyright (c) 2020 Mr Coxall All rights reserved
- *
- * Created by: Mr Coxall
- * Created on: Sep 2020
- * This is the Splash Scene
- */
+/* global phaser */
+// Created by: shem
+// Created on: May 2025
+// This is the splash scene for the game
 
 /**
- * This class is the Splash Scene.
+ * This class is the splash scene for the game
  */
 class SplashScene extends Phaser.Scene {
-    constructor () {
-      super({ key: 'splashScene' })
-    }
-  
-    init (data) {
-      this.cameras.main.setBackgroundColor('#ffffff')
-    }
-
-    preload () {
-      console.log('Splash Scene')
-    }
-  
-    create (data) {
-      // pass
-    }
-  
-    update (time, delta) {
-      // pass
-    }
+  constructor() {
+      super({ key: 'splashScene' });
   }
-  
+
+
+  init (data) {
+  this.cameras.main.setBackgroundColor("ffffff");
+  }
+
+  preload() {
+      console.log('Splash Scene');
+  }
+
+  create (data) {
+  }
+
+  update (time, delta) { 
+  }
+}
   export default SplashScene
-  
