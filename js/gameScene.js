d7f0d4bbe7d9ea25bec5924e48097e2c054cb8b7
Nebiyu Daniel <nebiyu.daniel@stu.ocsb.ca>
2025-05-27 15:04:38 +0000

    gh

--- js/gameScene.js
+++ js/gameScene.js
@@ -3,24 +3,33 @@
 // Created on: May 2025
 // This is the Game scene for the game
 
+import TitleScene from "./titleScene";
+
 /**
  * This class is the splash scene for the game
  */
 class GameScene extends Phaser.Scene {
     constructor() {
-        super({ key: 'gameScene' });
+        super({ key: 'gameScene' })
+
+        this.background = null
     }
   
   
     init (data) {
-    this.cameras.main.setBackgroundColor("AEA04B");
+    this.cameras.main.setBackgroundColor("#ffffff");
     }
   
     preload() {
-        console.log('Game Scene');
+        console.log('Game Scene')
+
+        this.preload.image('starBackground', 'assets/starBackground.png')
+
     }
   
-    create (data) {
+    create(data) {
+        this.background = this.add.image(0, 0, 'starBackground').setScale(2.0)
+        this.background.setOrigin(0, 0)
     }
   
     update (time, delta) { 
